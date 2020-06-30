2fcabbaa99747e3c0c388401252ebbb6b7735259
ForaceGump <themattavant@gmail.com>
2020-06-29 19:47:08 -0700

    Added a createID function so that I can add the DELETE method.

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -8,9 +8,19 @@ module.exports = function(app){
 
     app.post("/api/notes", function(req,res){
         const note = req.body;
+        note.id = 
         notes.push(note);
+        createID();
         fs.writeFileSync("./db/db.json", JSON.stringify(notes), function(err) {
             if (err) throw err;
         })
-    })
+    });
+
+    function createID(){
+        let i = 0;
+        notes.forEach(note => {
+            note.id = i;
+            i++;
+        });
+    }
 }
