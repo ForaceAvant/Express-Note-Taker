cf1dc006d105cd8d630de5461aa84d9fa2bc8cc1
ForaceGump <themattavant@gmail.com>
2020-06-29 19:09:05 -0700

    Added POST method to apiRoutes and also created htmlRoutes to test POST method

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -4,5 +4,13 @@ const fs = require("fs");
 module.exports = function(app){
     app.get("/api/notes", function(req, res){
         res.json(notes);
+    });
+
+    app.post("/api/notes", function(req,res){
+        const note = req.body;
+        notes.push(note);
+        fs.writeFileSync("./db/db.json", JSON.stringify(notes), function(err) {
+            if (err) throw err;
+        })
     })
 }
